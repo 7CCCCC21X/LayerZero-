02c7411afdbfa4e5737ddce90f1869bde8d7494e
7CCCCC21X <731495@qq.com>
2025-03-30 19:20:24 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -21,9 +21,7 @@ export default async function handler(req, res) {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-        "Accept": "*/*",
-        "Origin": "https://layer-zero-8po0p25nb-7ccccc21xs-projects.vercel.app",
-        "Referer": "https://layer-zero-8po0p25nb-7ccccc21xs-projects.vercel.app/",
+        "Accept": "*/*"
       }
     });
 
